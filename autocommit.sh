20a9cdcea66735863043c93d405bcdb333cbce2d
arielivandiaz <arielivandiaz@gmail.com>
2019-05-25 09:16:11 -0300

    v1.6

--- autocommit.sh
+++ autocommit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#node version/updateVersion.js
+node version/updateVersion.js
 version=$(cat version/version.json | jq '.version')
 rev=$(cat version/version.json | jq '.rev')
 message="v"${version}.${rev}
@@ -8,4 +8,4 @@ echo ${message}
 git status 
 git add -A
 git commit -m ${message}
-#git push  origin master
+git push  origin master
