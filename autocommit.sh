875cd969f0d17cb3ea2e4668950bd6ca927abde1
arielivandiaz <arielivandiaz@gmail.com>
2019-05-25 09:13:55 -0300

    {$message}

--- autocommit.sh
+++ autocommit.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-node /version/updateVersion.js
-version= cat /version/version.json | jq '.version'
-rev= cat /version/version.json | jq '.rev'
-message=$version.$rev
+#node version/updateVersion.js
+version=$(cat version/version.json | jq '.version')
+rev=$(cat version/version.json | jq '.rev')
+message=${version}.${rev}
+echo ${message}
 git status 
 git add -A
 git commit -m '{$message}'
-git push  origin master
+#git push  origin master
--- version/updateVersion.js
+++ version/updateVersion.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
 
+var file_path = 'version/version.json';
 
-fs.readFile('version.json', 'utf8', function readFileCallback(err, data) {
+fs.readFile(file_path, 'utf8', function readFileCallback(err, data) {
     if (err) {
         console.log(err);
     } else {
         obj = JSON.parse(data); 
         obj.rev += 1;
         json = JSON.stringify(obj); 
-        fs.writeFile('version.json', json, 'utf8', () => {
+        fs.writeFile(file_path, json, 'utf8', () => {
             console.log("Updated version: " +obj.version+"."+obj.rev);
         }); 
     }
