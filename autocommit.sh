d71f72ff2434a44d3a03671b7b250843e818898b
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2024-01-17 05:57:23 -0300

    v2.0.4

--- autocommit.sh
+++ autocommit.sh
@@ -2,8 +2,8 @@
 # https://github.com/arielivandiaz/auto-commit
 
 node version/updateVersion.js $1
-#version=$(cat version/version.json | jq '.version')
-version=$(cat package.json | jq '.version')
+#version=$(cat version/version.json | jq -r '.version')
+version=$(cat package.json | jq -r '.version')
 message="v"${version}
 echo ${message}
 git status 
